d97b6ebec63e3f4804beb5864c1c2e6a870f7ff6
Cenzo Truong <cenzo.truong@legato.co>
2025-04-15 13:01:46 +0700

    test

--- src/components/auth/sign-in-form.tsx
+++ src/components/auth/sign-in-form.tsx
@@ -46,6 +46,8 @@ export function SignInForm(): React.JSX.Element {
     formState: { errors },
   } = useForm<Values>({ resolver: zodResolver(schema) });
 
+  console.log('test');
+
   const onSubmit = React.useCallback(
     async (values: Values): Promise<void> => {
       setIsPending(true);
